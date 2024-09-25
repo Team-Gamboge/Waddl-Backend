c3d09e8f8b37805cada2ec296c99f65d2c533e55
Alan Davison <contact@decidedlyhuman.co.uk>
2024-09-25 15:58:25 +0100

    Initial commit.

---
+++ src/main/java/com/northcoders/gamboge/waddl_api/WaddlApiApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.gamboge.waddl_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaddlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaddlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/gamboge/waddl_api/WaddlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.gamboge.waddl_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaddlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
