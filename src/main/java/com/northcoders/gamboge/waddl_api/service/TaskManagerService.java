41a54cc6d0f18ac409642c45295b13d2f1d48591
Trntybth <trinitybooth89@gmail.com>
2024-09-27 16:04:37 +0100

    feat: added empty methods to service layer

--- src/main/java/com/northcoders/gamboge/waddl_api/service/TaskManagerService.java
+++ src/main/java/com/northcoders/gamboge/waddl_api/service/TaskManagerService.java
@@ -1,4 +1,18 @@
 package com.northcoders.gamboge.waddl_api.service;
 
+import com.northcoders.gamboge.waddl_api.model.Task;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public interface TaskManagerService {
+
+    List<Task> getAllTasks();
+    Optional<Task> getTaskById();
+    Task addTask(Task task);
+    Task updateTaskById(Long id, Task task);
+    void deleteTaskById();
+
 }
