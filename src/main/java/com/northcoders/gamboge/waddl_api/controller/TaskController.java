bf6de7f84697a7d0c374dde779eec88aaf86792d
Trntybth <trinitybooth89@gmail.com>
2024-09-27 11:38:15 +0100

    feat: Created controller layer

---
+++ src/main/java/com/northcoders/gamboge/waddl_api/controller/TaskController.java
@@ -0,0 +1,15 @@
+package com.northcoders.gamboge.waddl_api.controller;
+
+import com.northcoders.gamboge.waddl_api.service.TaskManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/waddl")
+public class TaskController {
+
+    @Autowired
+    TaskManagerService taskManagerService;
+
+}
